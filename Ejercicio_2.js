c07ec89fa36883273cd8afc155753124d5211d7a
Eliud Fuentes <79936847+EliudFuentes@users.noreply.github.com>
2023-02-03 00:10:00 -0600

    Add files via upload

---
+++ Ejercicio_2.js
@@ -0,0 +1,14 @@
+const nombre = "Eliud Francisco Fuentes Murillo";
+const edad = 22;
+const desarrollador = true;
+const date = new Date("July 06 2000");
+const libro = {
+    autor: "Eliud",
+    titulo:  "Libro de eliud",
+    numpaginas: 25,
+    pastaverde: true
+}
+
+const lista = [nombre, edad, desarrollador, date, libro]
+console.log(lista);
+
